60fd91fe4d8e662d89584bf6e59cb508772d7342
Brian Mills <brian@millsit.com>
2024-06-24 23:24:45 +1000

    Working on dev drive commands

--- setup-devdrives.ps1
+++ setup-devdrives.ps1
@@ -1,11 +1,48 @@
 
 # Setup Dev Drives
 Write-Output "Setting up Dev Drives"
+# Setup folders for Dev Drive Mount points
 New-Item -Path "C:\" -Name "DevDrives" -ItemType "directory"
 
-New-Item -Path "C:\DevDrives" -Name "Nuget" -ItemType "directory"
-
 # Requires HyperV Tools
+#diskpart /s ./diskpart1.txt
+
+# Mount NugetCache Disk
+$DiskName = "Test"
+$DiskSize = 52000
+$BasePath = "C:\DevDrives"
+
+# FUNCTION to create and mount dev drive
+# Setup Variables
+$MountPath = "$BasePath\$DiskName"
+$VhdPath = "$BasePath\$DiskName.vdhx"
+# Create the mount point folder
+New-Item -Path $BasePath -Name $DiskName -ItemType "directory"
+# Create the Vhdx disk image
+"CREATE VDISK FILE=`"$VhdPath`" MAXIMUM=$DiskSize TYPE=EXPANDABLE" | diskpart
+# Mount the Vhdx disk image
+Mount-DiskImage -ImagePath $VhdPath -NoDriveLetter
+# Get the disk number of the mounted VHD
+$Disk = Get-Disk | Where-Object { $_.Location -eq $VhdPath }
+# Initialize the disk
+Initialize-Disk -Number $Disk.Number -PartitionStyle GPT
+# Create a new partition and format it
+$Partition = New-Partition -DiskNumber $Disk.Number -UseMaximumSize -NoDriveLetter
+# Format the partition as a dev drive
+Format-Volume -Partition $Partition -DevDrive -NewFileSystemLabel $DiskName -Confirm:$false
+# Mount the partion to a path
+$Partition | Add-PartitionAccessPath -AccessPath $MountPath
+Write-Output "Done"
+
+
+
+
+
+
+
+
+
+
 # New-VHD -Path c:\DevDrives\NugetCache.vhdx -SizeBytes 50GB -Dynamic | 
 # Mount-VHD -Passthru | 
 Initialize-Disk -PartitionStyle GPT -FriendlyName NugetCacheDisk -Passthru | 
@@ -13,10 +50,11 @@ New-Partition -NoDriveLetter -GptType -UseMaximumSize |
 Format-Volume -DevDrive -Confirm:$false -FileSystemLabel NugetCache -Force
 
 
-diskpart /s ./diskpart1.txt
+Mount-DiskImage -ImagePath "C:\DevDrives\NugetCache.vhdx" -NoDriveLetter -PassThru | 
+Initialize-Disk -PartitionStyle GPT -FriendlyName NugetCacheDisk -Passthru | 
+New-Partition -UseMaximumSize | 
+Format-Volume -DevDrive -Confirm:$false -FileSystemLabel NugetCache -Force
 
-
-Mount-DiskImage -ImagePath "C:\DevDrives\NugetCache.vhdx" -NoDriveLetter -PassThru | Initialize-Disk -PartitionStyle GPT -FriendlyName NugetCacheDisk -Passthru | New-Partition -UseMaximumSize | Format-Volume -DevDrive -Confirm:$false -FileSystemLabel NugetCache -Force
 Mount-DiskImage -ImagePath "C:\DevDrives\NugetCache.vhdx" -NoDriveLetter -PassThru | Initialize-Disk -PartitionStyle GPT -FriendlyName NugetCacheDisk -Passthru | New-Partition -UseMaximumSize | Format-Volume -DevDrive -Confirm:$false -FileSystemLabel NugetCache -Force
 
 -DiskPath "C:\DevDrives\NugetCache.vhdx" -Path C:\DevDrives\Nuget
