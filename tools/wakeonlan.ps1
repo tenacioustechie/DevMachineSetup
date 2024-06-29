10e532d5d820b4328c2ebd7b43fa8972fd0dcc21
Brian Mills <brian@millsit.com>
2024-06-29 14:07:18 +1000

    Added wake on lan script

---
+++ tools/wakeonlan.ps1
@@ -0,0 +1,7 @@
+$Mac = "D8:BB:C1:CD:18:b9"
+$MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray * 16)
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+$UdpClient.Close()
